4ade56d19a83fbd043b18796e947aaf30e3de1ab
iHelp101 <william@ihelp101.com>
2015-09-29 17:21:03 -0400

    Code Updates

---
+++ app/src/main/java/com/ihelp101/instagram/Receiver.java
@@ -0,0 +1,13 @@
+package com.ihelp101.instagram;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class Receiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Hello", Toast.LENGTH_SHORT).show();
+    }
+}
