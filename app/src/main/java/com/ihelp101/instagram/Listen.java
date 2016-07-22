50fa8bf370ef8cdd8b65563994cf8ee88dddd8ed
iHelp101 <william@ihelp101.com>
2016-07-21 22:46:44 -0400

    Clear Out

--- app/src/main/java/com/ihelp101/instagram/Listen.java
+++
@@ -1,9 +0,0 @@
-package com.ihelp101.instagram;
-
-public interface Listen {
-    public abstract void init();
-
-    public abstract boolean shouldUserUpdate(String s, int i, String s1);
-
-    public abstract boolean canAutoUpdate(String s, int i);
-}
